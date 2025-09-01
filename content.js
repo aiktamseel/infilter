9905f46b3250729bd1179da62fb52282d75e505a
aiktamseel <tamseel.amd@gmail.com>
2025-09-01 11:33:32 +0500

    distribution mechanism

--- content.js
+++ content.js
@@ -3,6 +3,11 @@ const SELECTORS = {
     content: 'div.feed-shared-update-v2__description',
     header: 'div.relative',
     feed: 'div.scaffold-finite-scroll__content',
+    postTextBox: '.share-box-feed-entry__top-bar > button',
+    postTextField: '.ql-editor',
+    postButton: 'button.share-actions__primary-action',
+    postLinkClose: '.share-creation-state__preview-container-btn',
+    // postConfirmation: '.sharing-nba-framework__toast-v2 > div',
     dropdownbtn: 'div:has(> [aria-label="Sort order dropdown button"])',
     dropdownoptions: '.artdeco-dropdown__item',
     rightpanel: 'aside.scaffold-layout__aside',
@@ -26,108 +31,199 @@ const SELECTORS = {
     chatTitle: 'div.msg-cross-pillar-inbox-top-bar-wrapper__container > div > h1'
 };
 
-const HEADER_KEYWORDS = ['Promoted', 'ecommended for you', 'follows', 'follow ', 'course'];
-const DELAY = 30;
-const LOG_PREFIX = 'inFilter:';
+
+const SHARENUDGE = (postnum) => `
+<div style="margin: 2.5rem; font-size: 1.4rem;">
+  <p style="margin-bottom: 1.5rem; font-size: inherit">
+    You have removed ${postnum} unwanted posts thanks to inFilter. Why not share this awesome extension with your connections?
+  </p>
+  <div style="text-align: center;">
+    <button class="artdeco-button" data-action="inFilterShare" style="font-size: inherit">Share on Linkedin</button>
+  </div>
+</div>
+`;
+
+const SHARETEXT = (postnum) => `<p>Ever feel like your feed shows you more ads than actual content?</p><p>Or that you’re scrolling past things you’d never want to see in the first place? 👀</p><p><br></p><p>I came across a Chrome extension called inFilter that fixes exactly that:</p><p><br></p><p>&gt; Removes promoted posts 🚫</p><p>&gt; Lets you hide posts with specific keywords 🔑</p><p>&gt; Simplifies the UI for a distraction-free experience 👌</p><p><br></p><p>So far, I have saved ~${Math.round(postnum*0.23)} minutes and removed ${postnum} unwanted posts.</p><p><br></p><p>Sharing in case it helps others too:&nbsp;</p><p>https://chromewebstore.google.com/detail/infilter/lmlhegmkjkkeigpmgfgbdfdgpcocapeg</p><p><br></p><p><br></p><p><strong class="ql-hashtag" data-test-ql-hashtag="true">#productivity</strong> <strong class="ql-hashtag" data-test-ql-hashtag="true">#tools</strong> <strong class="ql-hashtag" data-test-ql-hashtag="true">#inFilter</strong></p>`;
+
+
+const HEADER_KEYWORDS = ['ecommended for you', 'follows', 'follow ', 'course'];
 
 let observer;
 let keywords = [];
+let autoSort;
+let minimalTheme;
+let removeRecommended;
 let removeSuggested;
+let totalPostsRemoved;
+let sessionPostsRemoved = 0;
 
-function log(message) {
-    console.log(`${LOG_PREFIX} ${message}`);
+let sharedOnLinkedin;
+let shareTotalPostLimit = 50;
+let shareSessionPostLimit = 6;
+let shareEveryNthPost = 4;
+let shareEveryNthPostSortOn = 4;
+let shareEveryNthPostSortOff = 12;
+
+let removeQueue = Promise.resolve();
+
+
+// Print timestamped logs
+function log(...messages) {
+    console.log(`inFilter: ${messages.join(' ')} [${(Date.now() / 1000).toFixed(3)}s]`);
 }
 
-function incrementPostCount() {
-    chrome.storage.sync.get(['totalPostsRemoved'], (data) => {
-        chrome.storage.sync.set({ totalPostsRemoved: (data.totalPostsRemoved || 0) + 1 });
+// Fetch settings from chrome storage & load to local vars
+function refreshSettings() {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get(['keywords', 'autoSort', 'minimalTheme', 'removeRecommended', 'removeSuggested', 'totalPostsRemoved', 'sharedOnLinkedin'], (data) => {
+            keywords = (data.keywords || []).map(str => str.toLowerCase());
+            autoSort = data.autoSort !== undefined ? data.autoSort : true;
+            minimalTheme = data.minimalTheme !== undefined ? data.minimalTheme : true;
+            removeRecommended = data.removeRecommended !== undefined ? data.removeRecommended : true;
+            removeSuggested = data.removeSuggested !== undefined ? data.removeSuggested : true;
+            totalPostsRemoved = data.totalPostsRemoved || 0;
+            sharedOnLinkedin = data.sharedOnLinkedin;
+            
+            if (autoSort) shareEveryNthPost = shareEveryNthPostSortOn; else shareEveryNthPost = shareEveryNthPostSortOff;
+
+            resolve();
+            log('autoSort =', autoSort);
+            log('minimalTheme =', minimalTheme);
+            log('removeRecommended =', removeRecommended);
+            log('removeSuggested =', removeSuggested);
+        });
     });
 }
 
+// Remove feed element (or replace with share nudge)
+function removeElement(element) {
+    removeQueue = removeQueue.then(() => {
+        return new Promise((resolve) => {
+            if (totalPostsRemoved < shareTotalPostLimit || sharedOnLinkedin ||
+                 sessionPostsRemoved < shareSessionPostLimit || sessionPostsRemoved % shareEveryNthPost !== 0) {
+                element.remove();
+                totalPostsRemoved++;
+                sessionPostsRemoved++;
+                log("Post removed. #", sessionPostsRemoved);
+                chrome.storage.sync.set({ totalPostsRemoved: totalPostsRemoved }, resolve);
+            } else {
+                element.innerHTML = SHARENUDGE(totalPostsRemoved);
+                sessionPostsRemoved++;
+                log("Post removed. #", sessionPostsRemoved);
+                const feedElement = document.querySelector(SELECTORS.feed);
+                if (!feedElement.hasAttribute('data-infilter-share-listener')) {
+                    log("Creating infilter share listener")
+                    feedElement.addEventListener('click', function(e) {
+                        if (e.target.dataset.action === 'inFilterShare') {
+                            inFilterShare();
+                        }
+                    });
+                    feedElement.setAttribute('data-infilter-share-listener', 'true');
+                } else log("infilter share listener already exists");
+                resolve();
+            }
+        });
+    }).catch(error => {
+        console.error('inFilter: Error in removeElement queue:', error);
+    });
+}
+
+// Draft linkedin post when share button clicked
+function inFilterShare() {
+    document.querySelector(SELECTORS.postTextBox).click();
+    waitForElement(SELECTORS.postTextField, (element) => {
+        element.innerHTML = SHARETEXT(totalPostsRemoved);
+        waitForElement(SELECTORS.postLinkClose, (closebtn) => closebtn.click());
+        
+        // Wait for post button and add click event listener
+        waitForElement(SELECTORS.postButton, (postButton) => {
+            postButton.addEventListener('click', function() {
+                if (document.querySelector(SELECTORS.postTextField).textContent.toLowerCase().includes('infilter')) {
+                    chrome.storage.sync.set({ sharedOnLinkedin: true }, () => {
+                        log("Share on Linkedin Successful");
+                    });
+                    sharedOnLinkedin = true;
+                } else {
+                    log("Share Unsuccesful - Post content doesn't include infilter: ", postText);
+                }
+            });
+        });
+    });
+}
+
+// Check if post meets removal criteria
 function checkPost(post) {
     const contentWrapper = post.querySelector(SELECTORS.content);
     const header = post.querySelector(SELECTORS.header);
     
-    if (header && HEADER_KEYWORDS.some(str => header.textContent.includes(str))) {
-        log("Promoted Post removed");
-        return true;
-    }
-    
-    if (removeSuggested && header?.textContent.includes('Suggested')) {
-        log("Suggested Post removed");
-        return true;
-    }
-    
-    if (contentWrapper && keywords.some(str => contentWrapper.textContent.toLowerCase().includes(str))) {
-        log("Blocked-keyword Post removed");
-        return true;
-    }
-
-    return false;
+    // Always remove promoted posts
+    return header?.textContent.includes('Promoted') ||
+        // Remove "recommended for you" if flag is set and post header includes HEADER_KEYWORDS
+        (removeRecommended && header && HEADER_KEYWORDS.some(str => header.textContent.includes(str))) ||
+        // Remove suggested if flag is set
+        (removeSuggested && header?.textContent.includes('Suggested')) ||
+        // Remove if post content includes blocked keywords
+        (contentWrapper && keywords.some(str => contentWrapper.textContent.toLowerCase().includes(str)));
 }
 
+// Check all posts and observe new loading posts
 function removePosts() {
     log("Check all posts");
     
-    chrome.storage.sync.get(['keywords', 'removeSuggested'], (data) => {
-        keywords = (data.keywords || []).map(str => str.toLowerCase());
-        removeSuggested = data.removeSuggested;
+    // Check existing posts
+    document.querySelectorAll(SELECTORS.post).forEach(post => {
+        if (checkPost(post)) removeElement(post);
+    });
 
-        document.querySelectorAll(SELECTORS.post).forEach(post => {
-            if (checkPost(post)) {
-                post.remove();
-                incrementPostCount();
-            }
-        });
-
-        if (observer) observer.disconnect();
-        
-        observer = new MutationObserver((mutations) => {
-            mutations.forEach(({ addedNodes }) => {
-                addedNodes.forEach(node => {
-                    if (node.nodeType === Node.ELEMENT_NODE && node.matches(SELECTORS.post) && checkPost(node)) {
-                        node.remove();
-                        incrementPostCount();
-                    }
-                });
+    // Observe new loading posts
+    if (observer) observer.disconnect();
+    observer = new MutationObserver((mutations) => {
+        mutations.forEach(({ addedNodes }) => {
+            addedNodes.forEach(node => {
+                if (node.nodeType === Node.ELEMENT_NODE && node.matches(SELECTORS.post) && checkPost(node)) {
+                    removeElement(node);
+                }
             });
         });
-        
-        observer.observe(document.querySelector(SELECTORS.feed), { childList: true, subtree: true });
     });
+    observer.observe(document.querySelector(SELECTORS.feed), { childList: true, subtree: true });
+
 }
 
-function waitForElement(selector, callback, maxAttempts = 50) {
+function waitForElement(selector, callback, maxAttempts = 200, delay = 30) {
     let attempts = 0;
     
     function check() {
         const element = document.querySelector(selector);
         if (element) {
+            log(selector, "found in attempt:", attempts);
             callback(element);
         } else if (attempts < maxAttempts) {
             attempts++;
-            setTimeout(check, DELAY);
+            setTimeout(check, delay);
+        } else {
+            log("Error -", selector, "not found.")
         }
     }
     
     check();
 }
 
+// Auto sort by Recent
 function switchToRecent() {
-    log("Switch to Recent called");
-    
+    log("Switching to Recent");
     waitForElement(SELECTORS.dropdownbtn, (sortButton) => {
         if (!sortButton.textContent.includes('Recent')) {
-            log("Switching")
-
+            
             sortButton.click();
             
-            waitForElement(SELECTORS.dropdownoptions, (dropdown) => {
+            waitForElement(SELECTORS.dropdownoptions, () => {
                 document.querySelectorAll(SELECTORS.dropdownoptions)[1].click();
             });
         }
     });
 }
+
 
 function createStyles() {
     const hiddenSelectors = ['rightpanel', 'leftpanel', 'premium2', 'premium3', 'chatAside', 'chatFilter', 'chatTitle']
@@ -146,7 +242,6 @@ function createStyles() {
 }
 
 function addProfileMenuItems() {
-    // Check if items have already been added
     waitForElement(SELECTORS.profileList, (profileList) => {
         if (profileList.hasAttribute('data-profile-items-added')) {
             return; // Items already added, skip
@@ -213,11 +308,17 @@ function cleanUI() {
 }
 
 
-function main() {
+async function main() {
+    await refreshSettings();
+    
+    if (minimalTheme) {
+        cleanUI()
+    } else {
+        document.getElementById('inFilter-styles')?.remove()
+    }
 
-    cleanUI();
     if (window.location.href.includes('linkedin.com/feed')) {
-        switchToRecent();
+        if (autoSort) switchToRecent();
         removePosts();
     }
 }
--- popup.js
+++ popup.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const keywordsTextarea = document.getElementById('keywords');
+    const autoSortCheckbox = document.getElementById('autoSort');
+    const minimalThemeCheckbox = document.getElementById('minimalTheme');
+    const removeRecommendedCheckbox = document.getElementById('removeRecommended');
     const removeSuggestedCheckbox = document.getElementById('removeSuggested');
     const saveButton = document.getElementById('saveKeywords');
     const statusDiv = document.getElementById('status');
     const postCountSpan = document.getElementById('postCount');
   
     // Load saved settings
-    chrome.storage.sync.get(['keywords', 'removeSuggested', 'totalPostsRemoved'], function(data) {
+    chrome.storage.sync.get(['keywords', 'autoSort', 'minimalTheme', 'removeRecommended', 'removeSuggested', 'totalPostsRemoved'], function(data) {
         keywordsTextarea.value = (data.keywords || []).join('\n');
-        removeSuggestedCheckbox.checked = data.removeSuggested || false;
+        autoSortCheckbox.checked = data.autoSort !== undefined ? data.autoSort : true;
+        minimalThemeCheckbox.checked = data.minimalTheme !== undefined ? data.minimalTheme : true;
+        removeRecommendedCheckbox.checked = data.removeRecommended !== undefined ? data.removeRecommended : true;
+        removeSuggestedCheckbox.checked = data.removeSuggested !== undefined ? data.removeSuggested : true;
         postCountSpan.textContent = data.totalPostsRemoved || 0;
     });
   
@@ -16,16 +22,19 @@ document.addEventListener('DOMContentLoaded', function() {
     saveButton.addEventListener('click', function() {
         let settings = {
             keywords: keywordsTextarea.value.split('\n').filter(k => k.trim() !== ''),
+            autoSort: autoSortCheckbox.checked,
+            minimalTheme: minimalThemeCheckbox.checked,
+            removeRecommended: removeRecommendedCheckbox.checked,
             removeSuggested: removeSuggestedCheckbox.checked
           };
         chrome.storage.sync.set(settings, function() {
             statusDiv.textContent = 'Changes saved!';
             setTimeout(() => { statusDiv.textContent = ''; }, 2000);
 
-        // Send cleanHomePage message
+        // Send reload infilter message
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             var activeTab = tabs[0];
-            if (activeTab && activeTab.url && activeTab.url.includes('linkedin.com/feed')) {
+            if (activeTab && activeTab.url && activeTab.url.includes('linkedin.com/')) {
               chrome.tabs.sendMessage(activeTab.id, { action: "inFilter" });
             }
         });
@@ -38,6 +47,13 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault(); // Prevent default action
         chrome.tabs.create({ url: this.href }); // Open link in new tab
       });
-    });  
-  });
+    });
 
+    // Share button functionality
+    document.getElementById('sharebtn').addEventListener('click', function() {
+      navigator.share({
+        title: 'inFilter - Chrome Extension for Cleaner Linkedin',
+        url: 'https://chromewebstore.google.com/detail/infilter/lmlhegmkjkkeigpmgfgbdfdgpcocapeg'
+      });
+    });
+  });
